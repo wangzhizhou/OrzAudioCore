595cfbd15516644c46024bbbf9f5b7aa2bf677c0
wangzhizhou <824219521@qq.com>
2026-07-19 00:02:17 +0800

    fix: support macOS 13 in native SDK

--- Sources/OrzAudioKitCXX/dispatch/build_info.generated.h
+++ Sources/OrzAudioKitCXX/dispatch/build_info.generated.h
@@ -1,1 +1,1 @@
-#define ORZ_BUILD_INFO_JSON "{\"sdk\":\"OrzAudioCore\",\"version\":\"1.1.0\",\"abi\":\"1.0\",\"fingerprint\":\"orzcore-1.1.0-m1\"}"
+#define ORZ_BUILD_INFO_JSON "{\"sdk\":\"OrzAudioCore\",\"version\":\"1.1.1\",\"abi\":\"1.0\",\"fingerprint\":\"orzcore-1.1.1-m1\"}"
